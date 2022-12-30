cce0b525a46eff01c89c9c91324d5c6650641aa2
sagar-nd <sagar@navadhiti.com>
2022-12-30 15:36:06 +0530

    Worked on fetching data and storing in an array to display in html webpage

--- covid-api-dataDisplay/app.js
+++ covid-api-dataDisplay/app.js
@@ -1,3 +1,47 @@
-fetch('https://data.covid19india.org/v4/min/data.min.json')
-  .then((response) => response.json())
-  .then((data) => console.log(data));
+fetch("https://data.covid19india.org/v4/min/data.min.json")
+  .then(response => response.json())
+  .then(data => {
+
+
+    let array1 = [];
+    let array2 = [];
+    let array3 = [];
+    let array4 = [];
+ 
+    for (var key of Object.keys(data)) {
+      for (var value of Object.values(data[key])) {
+        if (value.confirmed != undefined) {
+          array1.push(value.confirmed);
+        }
+        if (value.recovered != undefined) {
+          array2.push(value.recovered);
+        }
+        if (value.vaccinated1 != undefined) {
+          array3.push(value.vaccinated1);
+        }
+        if (value.vaccinated2 != undefined) {
+          array4.push(value.vaccinated2);
+        }
+      }
+    }
+    // console.log(array1);
+    const totalCases = array1.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    const recoveredCases = array2.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    const totalVaccinated1 = array3.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+    const totalVaccinated2 = array4.reduce(
+      (accumulator, currentValue) => accumulator + currentValue
+    );
+
+    const totalVaccinated = totalVaccinated1 + totalVaccinated2;
+    // console.log(totalVaccinated);
+    document.getElementById("TotalCases").innerHTML = totalCases;
+    document.getElementById("recovered").innerHTML = recoveredCases;
+    document.getElementById("totalVaccinated").innerHTML = totalVaccinated;
+    document.getElementById("band").innerHTML = totalVaccinated;
+  });
